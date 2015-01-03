c5e3f6ba097c08c8ad504d0ef22b6c97fa8ec7f0
remiGrandjean <remi1360@voila.fr>
2015-01-03 17:12:45 +0100

    Ajout du menu "Aide"

---
+++ VirtualMuseum/src/com/virtualMuseum/view/MyMenuHelp.java
@@ -0,0 +1,37 @@
+package com.virtualMuseum.view;
+
+import java.awt.Dimension;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class MyMenuHelp extends JFrame {
+	
+	public MyMenuHelp(){
+		JPanel panel = new JPanel();
+		this.setVisible(true);
+		this.setTitle("Aide");
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);	
+		this.setSize(600,150);
+		this.setLocationRelativeTo(null);
+		JLabel text=  new JLabel("Logiciel developper par Grandjean Rémi, Chevalier Benjamin et Quenon Thomas.");
+		JLabel text2=new JLabel("Réalisé au cour de la deuxième année de DUT informatique.");
+		JLabel text3 = new JLabel("Pour nous contacter en cas de problème d'utilisation du logiciel  : grandjeanremi0@gmail.com");
+		panel.setLayout(new BoxLayout(panel,BoxLayout.PAGE_AXIS));
+		panel.add(text);
+		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(text2);
+		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+		panel.add(text3);
+		this.add(panel);
+	}
+	
+
+
+
+
+}
