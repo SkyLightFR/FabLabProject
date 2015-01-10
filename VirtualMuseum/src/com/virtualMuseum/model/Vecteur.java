a8652514b2762c4be7756119a1bf1fdcf9954831
remiGrandjean <remi1360@voila.fr>
2015-01-10 13:33:08 +0100

    Ajout classe Vecteur

---
+++ VirtualMuseum/src/com/virtualMuseum/model/Vecteur.java
@@ -0,0 +1,14 @@
+package com.virtualMuseum.model;
+
+public class Vecteur {
+	public double X;
+	public double Y;
+	public double Z;
+	
+	public Vecteur(double X, double Y, double Z){
+		this.X=X;
+		this.Y=Y;
+		this.Z=Z;
+	}
+
+}
